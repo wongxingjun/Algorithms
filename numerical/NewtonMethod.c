9076c8ffa98d93e62902a1d20e4defeda0e4890c
wongxingjun <wongxingjun@126.com>
2014-11-12 21:35:21 +0800

    Newton method to solve quadratic equations

--- numerical/NewtonMethod.c
+++ numerical/NewtonMethod.c
@@ -1,4 +1,6 @@
-/*牛顿迭代法求一元二次方程的数值解*/
+/*牛顿迭代法求一元二次方程的数值解，
+  使用error控制精度
+ */
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
@@ -6,13 +8,13 @@ float func(float a,float b,float c,float x)
 {
     return a*x*x+b*x+c;
 }
-float newton(float a,float b,float c,float x0)
+float newton(float a,float b,float c,float x0,float error)
 {
     float x,temp;
     x=x0;
     temp=x0;
     x=-func(a,b,c,x)/(2*a*x+b)+x;
-    while(fabs(temp-x)>0.001)
+    while(fabs(temp-x)>error)
     {
         temp=x;
         x=-func(a,b,c,x)/(2*a*x+b)+x;
@@ -24,11 +26,10 @@ int main()
     float a,b,c,x0;
     scanf("%f%f%f",&a,&b,&c);
     float delta=b*b-4*a*c;
-    printf("delta=%.8f\n",delta);
     if(delta>1e-7)
     {
         scanf("%f",&x0);
-        printf("One of the solution is: %f\n",newton(a,b,c,x0));
+        printf("One of the solution is: %f\n",newton(a,b,c,x0,0.001));
     }
     else
         printf("There is no real sulution\n");

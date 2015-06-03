1a123d56ee5a83dc4b07d8a6c55138416e5798cf
wongxingjun <wongxingjun@126.com>
2015-06-03 21:17:47 +0800

    update

---
+++ leetcode/containsDup.py
@@ -0,0 +1,14 @@
+#Given an array of integers, find if the array contains any duplicates. Your function should return true if any value appears at least twice in the array, and it should return false if every element is distinct.
+class Solution:
+	# @param {integer[]} nums
+	# @return {boolean}
+	def containsDuplicate(self, nums):
+		if len(nums)<=1:
+			return False
+		d = {}
+		for i in nums:
+			if d.has_key(i)==False:
+				d[i]=1
+			else:
+				return True
+		return False

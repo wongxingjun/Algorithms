c3d8cf25034fc7adff51c661c894067659317724
wongxingjun <wongxingjun@126.com>
2015-10-27 19:25:02 +0800

    Populating Next Right Pointers in Each Node

---
+++ leetcode/populatingNextRightPointersinEachNode.cpp
@@ -0,0 +1,30 @@
+/**
+ * Definition for binary tree with next pointer.
+ * struct TreeLinkNode {
+ *  int val;
+ *  TreeLinkNode *left, *right, *next;
+ *  TreeLinkNode(int x) : val(x), left(NULL), right(NULL), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    void connect(TreeLinkNode *root) {
+        if(root==NULL)
+            return;
+        TreeLinkNode* cur=root;
+        TreeLinkNode* pre=root;
+        while(pre->left)
+        {
+            cur=pre;
+            while(cur)
+            {
+                cur->left->next=cur->right;
+                if(cur->next)
+                    cur->right->next=cur->next->left;
+                cur=cur->next;
+            }
+            pre=pre->left;
+        }
+        
+    }
+};

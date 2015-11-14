071fbeb0066fadb7818083d83ac4e22a9fec1dc0
wongxingjun <wongxingjun@126.com>
2015-11-14 17:01:34 +0800

    Pascal's Triangle

---
+++ leetcode/pascal'sTriangle.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    vector<vector<int>> generate(int numRows) {
+        vector<vector<int>> res;
+        if(numRows<=0)
+            return res;
+        vector<int> level1={1};
+        vector<int> level2={1,1};
+        res.push_back(level1);
+        if(numRows==1) return res;
+        res.push_back(level2);
+        if(numRows==2) return res;
+        int level=3;
+        vector<int> preLevel=level2;
+        vector<int> curLevel;
+        for(;level<=numRows;level++)
+        {
+            int i=1;
+            curLevel.push_back(1);
+            for(;i<level-1;i++)
+                curLevel.push_back(preLevel[i-1]+preLevel[i]);
+            curLevel.push_back(1);
+            res.push_back(curLevel);
+            preLevel=curLevel;
+            curLevel.erase(curLevel.begin(),curLevel.end());
+        }
+        return res;
+    }
+};
